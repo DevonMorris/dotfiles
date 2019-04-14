3ba433e0021c3bc23a6b965454c58db4aafb74a7
Devon Morris <devonmorris1992@gmail.com>
2019-04-13 18:57:06 -0600

    updated configure script

--- configure.sh
+++ configure.sh
@@ -5,7 +5,7 @@ NC='\033[0m'
 
 # Install Prerequisite apt packages
 echo -e "${CYAN}Installing prerequsite packages${NC}"
-sudo apt install feh zsh i3 i3lock git fonts-powerline wget pavucontrol zathura arandr ranger python3 libglib2.0-0 libgl1 libfontconfig1 libx11-xcb1 libxi6 libxrender1 libdbus-1-3 tox asciidoc silversearcher-ag fonts-font-awesome libncurses5-dev dbus-x11 xdotool
+sudo apt install feh zsh i3 i3lock git fonts-powerline wget pavucontrol zathura arandr ranger python3 libglib2.0-0 libgl1 libfontconfig1 libx11-xcb1 libxi6 libxrender1 libdbus-1-3 tox asciidoc silversearcher-ag fonts-font-awesome libncurses5-dev dbus-x11 xdotool xclip compton
 
 # Copying .gitconfig into home directory
 echo -e "${CYAN}Symlinking .gitconfig to home directory${NC}"
@@ -29,6 +29,15 @@ chsh -s $(which zsh)
 # Install Oh My Zsh
 echo -e "${CYAN}Installing Oh My Zsh${NC}"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -B)"
+
+# Symlink themes
+echo -e "${CYAN}Installing Gruvbox Themes${NC}"
+ln -svf `pwd`/oh-my-zsh-themes/gruvbox.zsh-theme $HOME/.config/.oh-my-zsh/molokai.zsh-theme
+ln -svf `pwd`/airline-themes/gruvbox.vim $HOME/.vim/plugged/vim-airline-themes/autoload/airline/themes/gruvbox.vim
+
+# Symlink neofetch config
+echo -e "${CYAN}Symlinking Neofetch Config${NC}"
+ln -svf `pwd`/.config/neofetch/config.conf $HOME/.config/neofetch/config.conf
 
 # Copy over .zshrc
 echo -e "${CYAN}Symlinking .zshrc to home directory${NC}"
