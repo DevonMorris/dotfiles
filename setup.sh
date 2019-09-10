11c704f2d49f26765096b77e70ca778046e6a1db
Devon Morris <devonmorris1992@gmail.com>
2019-09-10 19:30:34 -0400

    changed setup to just reset to HEAD, dangerous since it will kill all config files present in home dir

--- setup.sh
+++ setup.sh
@@ -6,13 +6,5 @@ git clone --bare git@github.com:DevonMorris/dotfiles.git $HOME/.dots
 function config {
    /usr/bin/git --git-dir=$HOME/.dots/ --work-tree=$HOME $@
 }
-mkdir -p .dotfiles-backup
-config checkout
-if [ $? = 0 ]; then
-  echo "Checked out config.";
-  else
-    echo "Backing up pre-existing dot files.";
-    config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .dotfiles-backup/{}
-fi;
-config checkout
+config reset --hard HEAD
 config config status.showUntrackedFiles no
