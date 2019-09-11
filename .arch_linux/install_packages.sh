acb070606f8fe05628fd6f64c0aac9de95fd90fc
Devon Morris <devonmorris1992@gmail.com>
2019-09-10 22:11:02 -0400

    update scripts for arch installation

---
+++ .arch_linux/install_packages.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+# Install arch packages
+sudo pacman -Syu
+sudo pacman -S --needed - < pkglistarch.txt
+
+# check if yay exists
+if ! [ -x "$(command -v yay)" ]; then
+  echo 'Yay is not installed; Installing now.'
+  mkdir $HOME/.sources
+  git clone https://aur.archlinux.org/yay.git $HOME/.sources/yay
+  pushd .
+  cd $HOME/.sources/yay
+  makepkg -si
+  popd
+fi
+
+# install AUR packages
+yay -Syu
+yay -S < pkglistyay.txt
