fd9659b55d5a76f98f69842d246471fe89b15605
Devon Morris <devonmorris1992@gmail.com>
2020-09-25 19:46:13 -0400

    setup.sh: update the comments in setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 # It should be as easy as running this setup file
-# Note: You will need to generate ssh keys and add them to github first
-# or if you are not me you will need to change the link to https
+# You'll probably need to change the remote to be able to push properly
 git clone --bare https://gitlab.com/DevonMorris/dotfiles.git $HOME/.dots
 function config {
    /usr/bin/env git --git-dir=$HOME/.dots/ --work-tree=$HOME $@
