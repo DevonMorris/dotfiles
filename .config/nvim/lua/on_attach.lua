c8b359f88782d276c0ef1c6e565b7ccb031c594f
Devon Morris <devonmorris1992@gmail.com>
2020-09-25 14:46:27 -0400

    nvim: only overwrite <c-]> when in an lsp enabled buffer

--- .config/nvim/lua/on_attach.lua
+++ .config/nvim/lua/on_attach.lua
@@ -1,6 +1,16 @@
 local M = {}
-M.on_attach = function()
-  require'diagnostic'.on_attach()
+local nvim_lsp = require'nvim_lsp'
+M.on_attach = function(_, buffnr)
   require'completion'.on_attach()
+  require'diagnostic'.on_attach()
+
+  -- Mappings.
+    local opts = { noremap=true, silent=true }
+    vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gd', '<Cmd>lua vim.lsp.buf.definition()<CR>', opts)
+    vim.api.nvim_buf_set_keymap(bufnr, 'n', 'K', '<Cmd>lua vim.lsp.buf.hover()<CR>', opts)
+    vim.api.nvim_buf_set_keymap(bufnr, 'n', '<c-k>', '<cmd>lua vim.lsp.util.show_line_diagnostics()<CR>', opts)
+    vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
+    vim.api.nvim_buf_set_keymap(bufnr, 'n', '<c-n>', '<cmd>NextDiagnosticCycle<CR>', opts)
+    vim.api.nvim_buf_set_keymap(bufnr, 'n', '<c-p>', '<cmd>PrevDiagnosticCycle<CR>', opts)
 end
 return M
