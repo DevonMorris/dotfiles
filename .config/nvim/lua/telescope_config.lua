26706f959ec39033245da9174fd7be261019830d
Devon Morris <devon.morris@torc.ai>
2021-03-16 11:08:25 -0400

    remove telescope git files

--- .config/nvim/lua/telescope_config.lua
+++ .config/nvim/lua/telescope_config.lua
@@ -59,37 +59,4 @@ M.find_dots = function(opts)
   }):find()
 end
 
--- Looks for git files, but falls back to normal files
-M.files = function(opts)
-  opts = opts or {}
-
-  vim.fn.system("git status")
-  local is_not_git = vim.v.shell_error > 0
-  if is_not_git then
-    require'telescope.builtin'.find_files(opts)
-    return
-  end
-
-  if opts.cwd then
-    opts.cwd = vim.fn.expand(opts.cwd)
-  else
-    --- Find root of git directory and remove trailing newline characters
-    opts.cwd = string.gsub(vim.fn.system("git rev-parse --show-toplevel"), '[\n\r]+', '')
-  end
-
-  -- By creating the entry maker after the cwd options,
-  -- we ensure the maker uses the cwd options when being created.
-  opts.entry_maker = opts.entry_maker or make_entry.gen_from_file(opts)
-
-  pickers.new(opts, {
-    prompt_title = 'Git File',
-    finder = finders.new_oneshot_job(
-      { "git", "ls-files", "--recurse-submodules" },
-      opts
-    ),
-    previewer = previewers.cat.new(opts),
-    sorter = conf.file_sorter(opts),
-  }):find()
-end
-
 return M
