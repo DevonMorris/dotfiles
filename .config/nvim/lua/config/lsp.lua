67df318d24eb8f0d93ad600c53fa3cdee630eed7
Devon Morris <devonmorris1992@gmail.com>
2021-03-17 00:03:19 -0400

    sumneko lua server

--- .config/nvim/lua/config/lsp.lua
+++ .config/nvim/lua/config/lsp.lua
@@ -69,7 +69,35 @@ nvim_lsp.clangd.setup{on_attach=on_attach,
 --nvim_lsp.vimls.setup{on_attach=on_attach}
 
 --Lua config
---nvim_lsp.sumneko_lua.setup{on_attach=on_attach}
+-- set the path to the sumneko installation; if you previously installed via the now deprecated :LspInstall, use
+local sumneko_root_path = vim.fn.stdpath('cache')..'/nvim_lsp/sumneko_lua/lua-language-server'
+local sumneko_binary = sumneko_root_path.."/bin/Linux/lua-language-server"
+
+nvim_lsp.sumneko_lua.setup {
+  on_attach=on_attach,
+  cmd = {sumneko_binary, "-E", sumneko_root_path .. "/main.lua"};
+  settings = {
+    Lua = {
+      runtime = {
+        -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+        version = 'LuaJIT',
+        -- Setup your lua path
+        path = vim.split(package.path, ';'),
+      },
+      diagnostics = {
+        -- Get the language server to recognize the `vim` global
+        globals = {'vim'},
+      },
+      workspace = {
+        -- Make the server aware of Neovim runtime files
+        library = {
+          [vim.fn.expand('$VIMRUNTIME/lua')] = true,
+          [vim.fn.expand('$VIMRUNTIME/lua/vim/lsp')] = true,
+        },
+      },
+    },
+  },
+}
 
 --Javascript/Typescript
 nvim_lsp.tsserver.setup{on_attach=on_attach}
