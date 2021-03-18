d1b28131bf8b1c2385f71bdd8493a09591a9d1c5
Devon Morris <devonmorris1992@gmail.com>
2021-03-17 23:07:52 -0400

    fix some file markers in init.lua

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,9 +1,13 @@
--- Neovim init.vim
--- author: Devon Morris
--- contact: devonmorris1992@gmail.com
--- date: Wed Mar 17 21:40:09 EDT 2021
--- Note: everything in this file should be neovim or plugin specific
--- for everything regarding plain vim config check out ~/.vimrc
+--[[
+Neovim init.lua
+author: Devon Morris
+contact: devonmorris1992@gmail.com
+date: Wed Mar 17 21:40:09 EDT 2021
+Note: everything in this file should be neovim or plugin specific
+for everything regarding plain vim config check out ~/.vimrc
+]]
+
+-- Source vimrc for neovim/plugin agnostic config
 vim.cmd'source ~/.vimrc'
 
 -- Utility functions to use from cmd
