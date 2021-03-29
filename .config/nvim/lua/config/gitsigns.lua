31a9eee6647b69827c719f49dc0085987ceca65b
Devon Morris <devon.morris@torc.ai>
2021-03-29 17:59:07 -0400

    <c-g> mappings for gitsigns

--- .config/nvim/lua/config/gitsigns.lua
+++ .config/nvim/lua/config/gitsigns.lua
@@ -1,1 +1,21 @@
-require'gitsigns'.setup{}
+require'gitsigns'.setup{
+  keymaps = {
+    -- Default keymap options
+    noremap = true,
+    buffer = true,
+
+    ['n ]c'] = { expr = true, "&diff ? ']c' : '<cmd>lua require\"gitsigns\".next_hunk()<CR>'"},
+    ['n [c'] = { expr = true, "&diff ? '[c' : '<cmd>lua require\"gitsigns\".prev_hunk()<CR>'"},
+
+    ['n <C-G>hs'] = '<cmd>lua require"gitsigns".stage_hunk()<CR>',
+    ['n <C-G>hu'] = '<cmd>lua require"gitsigns".undo_stage_hunk()<CR>',
+    ['n <C-G>hr'] = '<cmd>lua require"gitsigns".reset_hunk()<CR>',
+    ['n <C-G>hR'] = '<cmd>lua require"gitsigns".reset_buffer()<CR>',
+    ['n <C-G>hp'] = '<cmd>lua require"gitsigns".preview_hunk()<CR>',
+    ['n <C-G>hb'] = '<cmd>lua require"gitsigns".blame_line()<CR>',
+
+    -- Text objects
+    ['o ih'] = ':<C-U>lua require"gitsigns".select_hunk()<CR>',
+    ['x ih'] = ':<C-U>lua require"gitsigns".select_hunk()<CR>'
+  },
+}
--- .config/nvim/lua/config/lsp.lua
+++ .config/nvim/lua/config/lsp.lua
@@ -79,7 +79,7 @@ nvim_lsp.clangd.setup{on_attach=clangd_on_attach,
 local sumneko_root_path = vim.fn.stdpath('cache')..'/nvim_lsp/sumneko_lua/lua-language-server'
 local sumneko_binary = sumneko_root_path.."/bin/Linux/lua-language-server"
 
-nvim_lsp.sumneko_lua.setup {
+--[[ nvim_lsp.sumneko_lua.setup {
   on_attach=on_attach,
   cmd = {sumneko_binary, "-E", sumneko_root_path .. "/main.lua"};
   settings = {
@@ -103,7 +103,7 @@ nvim_lsp.sumneko_lua.setup {
       },
     },
   },
-}
+} ]]
 
 --Javascript/Typescript
 nvim_lsp.tsserver.setup{on_attach=on_attach}
