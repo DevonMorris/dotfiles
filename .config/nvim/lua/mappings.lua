3f7de84d314474a73f6a95c2d9f7cada96883993
Devon Morris <devonmorris1992@gmail.com>
2021-03-31 22:16:59 -0400

    center when going through qf list

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -8,13 +8,13 @@ set_keymap('n', '<C-H>', '<C-^>', opts)
 set_keymap('n', '<leader>w', '<Cmd>DockerToolsToggle<CR>', opts)
 
 -- Quickfix
-set_keymap('n', '<C-N>', '<Cmd>cn<CR>', opts)
-set_keymap('n', '<C-P>', '<Cmd>cp<CR>', opts)
+set_keymap('n', '<C-N>', '<Cmd>cn<CR>zz', opts)
+set_keymap('n', '<C-P>', '<Cmd>cp<CR>zz', opts)
 set_keymap('n', '<C-C>', '<Cmd>ccl<Bar>lcl<CR>', opts)
 
 -- LocList
-set_keymap('n', '<C-J>', '<Cmd>lnext<CR>', opts)
-set_keymap('n', '<C-K>', '<Cmd>lprev<CR>', opts)
+set_keymap('n', '<C-J>', '<Cmd>lnext<CR>zz', opts)
+set_keymap('n', '<C-K>', '<Cmd>lprev<CR>zz', opts)
 
 -- Execute some lua
 set_keymap('n', '<leader>l', ':lua ', { noremap = true, silent = false})
