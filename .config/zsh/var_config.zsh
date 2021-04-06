973643091e0b34992bf419494b5870dd57c99204
Devon Morris <devon.morris@torc.ai>
2021-04-06 15:55:46 -0400

    update path to include scripts dir

--- .config/zsh/var_config.zsh
+++ .config/zsh/var_config.zsh
@@ -7,9 +7,10 @@ export MANPAGER='nvim +Man!'
 # Add relevant directories to path
 export PATH=$HOME/.local/bin:$PATH
 export PATH=/snap/bin:$PATH
+export PATH=$HOME/scripts:$PATH
 
 # Add ccache support automatically if installed
 if which ccache > /dev/null 2>&1; then
-  export CC="ccache gcc"
-  export CXX="ccache g++"
+  #export CC="ccache gcc"
+  #export CXX="ccache g++"
 fi
