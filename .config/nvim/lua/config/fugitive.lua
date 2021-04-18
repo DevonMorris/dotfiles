b77864e6c8d5c472934514b4e688a5350e9d3f74
Devon Morris <devon.morris@torc.ai>
2021-04-18 15:28:28 -0400

    Add keybinding for git fetch

--- .config/nvim/lua/config/fugitive.lua
+++ .config/nvim/lua/config/fugitive.lua
@@ -1,3 +1,4 @@
 -- Vim Fugitive
 vim.api.nvim_set_var("fugitive_no_maps", 1)
 vim.api.nvim_set_keymap('n', '<leader>gb', '<Cmd>Git blame<CR>', opts)
+vim.api.nvim_set_keymap('n', '<C-G>f', '<Cmd>Git fetch<CR>', opts)
