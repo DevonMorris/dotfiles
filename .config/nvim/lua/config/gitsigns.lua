553d632cddbdbd37c204044c55e1987e9824bb36
Devon Morris <devonmorris1992@gmail.com>
2021-09-15 19:20:51 -0400

    tweaking git mappings again

--- .config/nvim/lua/config/gitsigns.lua
+++ .config/nvim/lua/config/gitsigns.lua
@@ -4,18 +4,17 @@ require'gitsigns'.setup{
     noremap = true,
     buffer = true,
 
-    ['n ]h'] = { expr = true, "&diff ? ']c' : '<cmd>lua require\"gitsigns\".next_hunk()<CR>'"},
-    ['n [h'] = { expr = true, "&diff ? '[c' : '<cmd>lua require\"gitsigns\".prev_hunk()<CR>'"},
+    ['n ]g'] = { expr = true, "&diff ? ']c' : '<cmd>lua require\"gitsigns\".next_hunk()<CR>'"},
+    ['n [g'] = { expr = true, "&diff ? '[c' : '<cmd>lua require\"gitsigns\".prev_hunk()<CR>'"},
 
-    ['n <C-G>hs'] = '<cmd>lua require"gitsigns".stage_hunk()<CR>',
-    ['n <C-G>hu'] = '<cmd>lua require"gitsigns".undo_stage_hunk()<CR>',
-    ['n <C-G>hr'] = '<cmd>lua require"gitsigns".reset_hunk()<CR>',
-    ['n <C-G>hR'] = '<cmd>lua require"gitsigns".reset_buffer()<CR>',
-    ['n <C-G>hp'] = '<cmd>lua require"gitsigns".preview_hunk()<CR>',
-    ['n <C-G>hb'] = '<cmd>lua require"gitsigns".blame_line()<CR>',
+    ['n <C-G>a'] = '<cmd>lua require"gitsigns".stage_hunk()<CR>',
+    ['n <C-G>A'] = '<cmd>lua require"gitsigns".stage_buffer()<CR>',
+    ['n <C-G>u'] = '<cmd>lua require"gitsigns".undo_stage_hunk()<CR>',
+    ['n <C-G>r'] = '<cmd>lua require"gitsigns".reset_hunk()<CR>',
+    ['n <C-G>R'] = '<cmd>lua require"gitsigns".reset_buffer()<CR>',
 
     -- Text objects
-    ['o ih'] = ':<C-U>lua require"gitsigns".select_hunk()<CR>',
-    ['x ih'] = ':<C-U>lua require"gitsigns".select_hunk()<CR>'
+    ['o ig'] = ':<C-U>lua require"gitsigns".select_hunk()<CR>',
+    ['x ig'] = ':<C-U>lua require"gitsigns".select_hunk()<CR>'
   },
 }
