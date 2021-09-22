df27b02b1653c3031316aa71f43cf7ab288c9c4c
Devon Morris <devonmorris1992@gmail.com>
2021-09-21 23:49:22 -0400

    fix prompt variable

--- .shell.d/prompt.bash
+++ .shell.d/prompt.bash
@@ -10,7 +10,7 @@ export PS1P=$'\033[31m'
 export PS1U=$PS1P
 
 if [[ $EUID == 0 ]]; then
-  export PS1='\[${PS1P}\]\u\[${PS1a}\]@\[${PS1h}\]\h\[${PS1c}\]:\[${PS1w}\]\W$(__git_ps1 "\[${PS1g}\](\[${gruv_purple}\]%s\[${PS1g}\])")\[$PS1d\]#\[\033[00m\] '
+  export PS1='\[${PS1P}\]\u\[${PS1a}\]@\[${PS1h}\]\h\[${PS1c}\]:\[${PS1w}\]\W$(__git_ps1 "\[${PS1g}\](\[${PS1u}\]%s\[${PS1g}\])")\[$PS1d\]#\[\033[00m\] '
 else
-  export PS1='\[${PS1u}\]\u\[${PS1a}\]@\[${PS1h}\]\h\[${PS1c}\]:\[${PS1w}\]\W$(__git_ps1 "\[${PS1g}\](\[${gruv_purple}\]%s\[${PS1g}\])")\[$PS1d\]$\[\033[00m\] '
+  export PS1='\[${PS1u}\]\u\[${PS1a}\]@\[${PS1h}\]\h\[${PS1c}\]:\[${PS1w}\]\W$(__git_ps1 "\[${PS1g}\](\[${PS1u}\]%s\[${PS1g}\])")\[$PS1d\]$\[\033[00m\] '
 fi
