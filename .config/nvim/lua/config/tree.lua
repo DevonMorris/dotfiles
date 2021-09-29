b07084e4a35a94ac776c6917b9151021e535dcde
Devon Morris <devon.morris@torc.ai>
2021-09-29 09:23:07 -0400

    update nvim tree for plugin updates

--- .config/nvim/lua/config/tree.lua
+++ .config/nvim/lua/config/tree.lua
@@ -3,7 +3,11 @@ local set_keymap = vim.api.nvim_set_keymap
 
 set_keymap('n', '<leader>ex', ':NvimTreeToggle<CR>', opts)
 
-vim.g.nvim_tree_width = 40
-vim.g.nvim_tree_auto_open = 0
-vim.g.nvim_tree_auto_close = 1
 vim.g.nvim_tree_quit_on_open = 1
+require'nvim-tree'.setup{
+  auto_close = true,
+  auto_open = false,
+  view = {
+    width = 40
+  }
+}
