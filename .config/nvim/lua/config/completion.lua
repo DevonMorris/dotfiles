0c158409386aad53c9b20ee58ec075eca8c553b0
Devon Morris <devon.morris@torc.ai>
2021-10-04 14:08:41 -0400

    remove configuration for completion old

--- .config/nvim/lua/config/completion.lua
+++ .config/nvim/lua/config/completion.lua
@@ -6,6 +6,3 @@ cmp.setup{
         { name = "neorg" }
   }
 }
-vim.api.nvim_set_var('completion_matching_strategy_list', {'exact', 'substring', 'fuzzy'})
-vim.api.nvim_set_var('completion_matching_ignore_case', 1)
-vim.api.nvim_set_var('completion_enable_auto_hover', 1)
