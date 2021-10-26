0e44b20467d6926b3fdc0d068d000426e2be6550
Devon Morris <devonmorris1992@gmail.com>
2021-10-26 19:20:54 -0400

    remove unused tags awesome

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -213,7 +213,7 @@ awful.screen.connect_for_each_screen(function(s)
     set_wallpaper(s)
 
     -- Each screen has its own tag table.
-    awful.tag({ "Term", "Viz", "Pres", "Web", "Msg", "Tunes", "Misc"}, s, awful.layout.layouts[1])
+    awful.tag({"Term", "Viz", "Tunes", "Web", "Msg"}, s, awful.layout.layouts[1])
 
     -- Create a promptbox for each screen
     s.mypromptbox = awful.widget.prompt()
