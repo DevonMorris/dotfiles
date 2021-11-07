c8c8302577cde7ce3f49cda587893b8daa9c856b
Devon Morris <devonmorris1992@gmail.com>
2021-11-07 11:34:48 -0500

    revert init dotfiles script

--- scripts/init_dotfiles.sh
+++ scripts/init_dotfiles.sh
@@ -5,16 +5,7 @@ git clone --bare https://gitlab.com/DevonMorris/dotfiles.git $HOME/.dots
 function dots {
    /usr/bin/env git --git-dir=$HOME/.dots/ --work-tree=$HOME $@
 }
-mkdir -p $HOME/.dots-bak
-dots checkout
-if [ $? = 0 ]; then
-  echo "Checked out dotfiles.";
-  else
-    echo "Backing up pre-existing dot files.";
-    dots checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .dots-bak/{}
-fi;
-dots checkout
+dots reset --hard HEAD
 dots config status.showUntrackedFiles no
 
-# Symlink scripts so that they are on path for most things
 ln -sf "$PWD/scripts" "$HOME/.local/bin/scripts"
