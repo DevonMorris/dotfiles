3a3c803154374295e62bd6c7a61644e05da17419
Devon Morris <devonmorris1992@gmail.com>
2021-11-08 09:57:44 -0500

    add missing telescope.lua

---
+++ .config/nvim/lua/devo/telescope.lua
@@ -0,0 +1,33 @@
+local pickers = require('telescope.pickers')
+local finders = require('telescope.finders')
+local make_entry = require('telescope.make_entry')
+local previewers = require('telescope.previewers')
+local utils = require('telescope.utils')
+local conf = require('telescope.config').values
+
+M = {}
+
+M.find_dots = function(opts)
+  opts.path_display = utils.get_default(opts.path_display, {"shorten"})
+  opts = opts or {}
+
+  opts.cwd = os.getenv("HOME")
+  -- By creating the entry maker after the cwd options,
+  -- we ensure the maker uses the cwd options when being created.
+  opts.entry_maker = opts.entry_maker or make_entry.gen_from_file(opts)
+
+  pickers.new(opts, {
+    prompt_title = '~~ Dotfiles ~~',
+    finder = finders.new_oneshot_job(
+      { "git",
+      "--git-dir="..os.getenv("HOME").."/.dots/",
+      "--work-tree="..os.getenv("HOME"),
+      "ls-tree", "--full-tree", "-r", "--name-only", "HEAD" },
+      opts
+    ),
+    previewer = previewers.vim_buffer_cat.new(opts),
+    sorter = conf.file_sorter(opts),
+  }):find()
+end
+
+return M
