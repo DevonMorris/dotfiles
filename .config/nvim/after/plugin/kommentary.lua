695354f1f3d3a605769794104cf1498f8d5e9fd3
Devon Morris <devonmorris1992@gmail.com>
2021-11-12 08:22:10 -0500

    single line comments in kommentary

--- .config/nvim/after/plugin/kommentary.lua
+++ .config/nvim/after/plugin/kommentary.lua
@@ -1,3 +1,6 @@
+require('kommentary.config').configure_language("default", {
+    prefer_single_line_comments = true,
+})
 vim.api.nvim_set_keymap("n", "<leader>cc", "<Plug>kommentary_line_increase", {})
 vim.api.nvim_set_keymap("n", "<leader>cu", "<Plug>kommentary_line_decrease", {})
 vim.api.nvim_set_keymap("n", "<leader>c", "<Plug>kommentary_motion_default", {})
