2eee1ae57b50ba652b64ed0f9090cfa9b829eb16
Devon Morris <devonmorris1992@gmail.com>
2021-11-30 20:29:46 -0500

    switch awesome theme to gruvbox

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -57,7 +57,7 @@ end
 
 -- {{{ Variable definitions
 -- Themes define colours, icons, font and wallpapers.
-beautiful.init("~/.config/awesome/themes/melange/theme.lua")
+beautiful.init("~/.config/awesome/themes/gruvbox_material/theme.lua")
 
 -- This is used later as the default terminal and editor to run.
 terminal = "st"
