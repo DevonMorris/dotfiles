21ee69ebf2c4c8e028c9564dec73298887e6a56d
Devon Morris <devonmorris1992@gmail.com>
2022-02-20 21:56:55 -0500

    clean up some mappings

--- .config/nvim/lua/devo/mappings.lua
+++ .config/nvim/lua/devo/mappings.lua
@@ -9,13 +9,6 @@ set_keymap('n', '<C-N>', '<Cmd>cn<CR>zz', opts)
 set_keymap('n', '<C-P>', '<Cmd>cp<CR>zz', opts)
 set_keymap('n', '<C-C>', '<Cmd>ccl<Bar>lcl<CR>', opts)
 
--- LocList
-set_keymap('n', '<C-J>', '<Cmd>lnext<CR>zz', opts)
-set_keymap('n', '<C-K>', '<Cmd>lprev<CR>zz', opts)
-
--- Search clear
-set_keymap('n', '<C-L>', '<Cmd>nohlsearch<CR>', opts)
-
 -- Y act like D and C
 set_keymap('n', 'Y', 'y$', opts)
 
