f706b95cb37bb2de6f0fbe85a2f9db4c4d593616
Devon Morris <devonmorris1992@gmail.com>
2022-09-27 20:19:10 -0400

    add starship prompt if available

--- .shell.d/prompt.bash
+++ .shell.d/prompt.bash
@@ -14,3 +14,5 @@ if [[ $EUID == 0 ]]; then
 else
   export PS1='\[${PS1u}\]\u\[${PS1a}\]@\[${PS1h}\]\h\[${PS1c}\]:\[${PS1w}\]\W$(__git_ps1 "\[${PS1g}\](\[${PS1u}\]%s\[${PS1g}\])")\[$PS1d\]$\[\033[00m\] '
 fi
+
+which starship &>/dev/null && eval "$(starship init bash)"
