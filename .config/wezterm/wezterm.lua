f46cf96557d8b171c57aeb7d569e61161619548d
Devon Morris <devonmorris1992@gmail.com>
2022-10-15 09:00:35 -0400

    stop wezterm from checking for updates

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -5,9 +5,11 @@ local keys = {
 
 return {
   font = wezterm.font 'Fira Code',
-  color_scheme = "Gruvbox dark, hard (base16)",
+  color_scheme = "Gruvbox Dark",
   enable_tab_bar = false,
   adjust_window_size_when_changing_font_size = false,
   warn_about_missing_glyphs = false,
+  check_for_updates = false,
+  show_update_window = false,
   keys = keys
 }
