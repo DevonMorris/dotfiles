5ee77c1c4f7961504c558138e26214c6320b3fb3
Devon Morris <devonmorris1992@gmail.com>
2023-01-21 11:22:25 -0500

    Change init dotfiles to use stow

--- scripts/init_dotfiles.sh
+++ scripts/init_dotfiles.sh
@@ -1,11 +1,19 @@
 #!/usr/bin/env bash
 # It should be as easy as running this setup file
 # You'll probably need to change the remote to be able to push properly
-git clone --bare https://gitlab.com/DevonMorris/dotfiles.git $HOME/.dots
-function dots {
-   /usr/bin/env git --git-dir=$HOME/.dots/ --work-tree=$HOME $@
-}
-dots reset --hard HEAD
-dots config status.showUntrackedFiles no
+# Requires git and stow to work properly
 
-ln -sf "$PWD/scripts" "$HOME/.local/bin/scripts"
+if ! command -v git &> /dev/null
+then
+  echo "Please install git"
+  exit
+fi
+
+if ! command -v stow &> /dev/null
+then
+  echo "Please install stow"
+  exit
+fi
+
+git clone https://gitlab.com/DevonMorris/dotfiles.git $HOME/.dots
+cd $HOME/.dots && stow -vvv -t $HOME --adopt .
