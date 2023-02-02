7a63047bdcede3ecf91fc6f6879642631212979f
Devon Morris <devonmorris1992@gmail.com>
2023-02-02 10:58:04 -0500

    Add dock information to prompt if inside docker

--- .shell.d/prompt.bash
+++ .shell.d/prompt.bash
@@ -8,11 +8,14 @@ export PS1d=$'\033[38;2;110;110;110m'
 export PS1p=$PS1u
 export PS1P=$'\033[31m'
 export PS1U=$PS1P
-
-if [[ $EUID == 0 ]]; then
-  export PS1='\[${PS1P}\]\u\[${PS1a}\]@\[${PS1h}\]\h\[${PS1c}\]:\[${PS1w}\]\W$(__git_ps1 "\[${PS1g}\](\[${PS1u}\]%s\[${PS1g}\])")\[$PS1d\]#\[\033[00m\] '
+if [[ -f "/.dockerenv" ]]; then
+  export DOCK="(docked)"
 else
-  export PS1='\[${PS1u}\]\u\[${PS1a}\]@\[${PS1h}\]\h\[${PS1c}\]:\[${PS1w}\]\W$(__git_ps1 "\[${PS1g}\](\[${PS1u}\]%s\[${PS1g}\])")\[$PS1d\]$\[\033[00m\] '
+  export DOCK=""
 fi
 
-which starship &>/dev/null && eval "$(starship init bash)"
+if [[ $EUID == 0 ]]; then
+  export PS1='$DOCK\[${PS1P}\]\u\[${PS1a}\]@\[${PS1h}\]\h\[${PS1c}\]:\[${PS1w}\]\W$(__git_ps1 "\[${PS1g}\](\[${PS1u}\]%s\[${PS1g}\])")\[$PS1d\]#\[\033[00m\] '
+else
+  export PS1='$DOCK\[${PS1u}\]\u\[${PS1a}\]@\[${PS1h}\]\h\[${PS1c}\]:\[${PS1w}\]\W$(__git_ps1 "\[${PS1g}\](\[${PS1u}\]%s\[${PS1g}\])")\[$PS1d\]$\[\033[00m\] '
+fi
