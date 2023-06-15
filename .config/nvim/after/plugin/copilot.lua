36ac9de20bc4cfaf4e075cfe0d548d192c1ca91a
Devon Morris <devonmorris1992@gmail.com>
2023-06-14 21:21:38 -0400

    add copilot lua

---
+++ .config/nvim/after/plugin/copilot.lua
@@ -0,0 +1,13 @@
+require("copilot").setup{
+  panel = {
+    enabled = false,
+  },
+  suggestion = {
+    enabled = true,
+    auto_trigger = true,
+    keymap = {
+      accept = "<tab>",
+      accept_line = true,
+    },
+  },
+}
--- .config/nvim/after/plugin/statusline.lua
+++ .config/nvim/after/plugin/statusline.lua
@@ -1,14 +1,3 @@
-local gps = require'nvim-gps'
-
-gps.setup({
-	icons = {
-		["class-name"] = ' ',      -- Classes and class-like objects
-		["function-name"] = ' ',   -- Functions
-		["method-name"] = ' '      -- Methods (functions inside class-like objects)
-	},
-	separator = ' > ',
-})
-
 local lualine = require'lualine'
 
 lualine.setup{
