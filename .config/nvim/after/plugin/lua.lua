fb148e2e6c18e092b04719f3836ef669ba37493f
Devon Morris <devonmorris1992@gmail.com>
2023-06-28 08:00:02 -0400

    add lua ls

---
+++ .config/nvim/after/plugin/lua.lua
@@ -0,0 +1,28 @@
+local devo_lsp = require'devo.lsp'
+local nvim_lsp = require'lspconfig'
+local nvim_lsp_util = require'lspconfig/util'
+local on_attach = devo_lsp.on_attach
+local capabilities = devo_lsp.capabilities
+
+require'lspconfig'.lua_ls.setup {
+  settings = {
+    Lua = {
+      runtime = {
+        -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+        version = 'LuaJIT',
+      },
+      diagnostics = {
+        -- Get the language server to recognize the `vim` global
+        globals = {'vim'},
+      },
+      workspace = {
+        -- Make the server aware of Neovim runtime files
+        library = vim.api.nvim_get_runtime_file("", true),
+      },
+      -- Do not send telemetry data containing a randomized but unique identifier
+      telemetry = {
+        enable = false,
+      },
+    },
+  },
+}
