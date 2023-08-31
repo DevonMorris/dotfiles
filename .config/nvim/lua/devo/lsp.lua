6ef262b7fabfec361e8ee8b9990ff1e3c7a46a62
Devon Morris <devonmorris1992@gmail.com>
2023-08-31 16:54:57 -0400

    Remove annoying parts of errors in lsp

--- .config/nvim/lua/devo/lsp.lua
+++ .config/nvim/lua/devo/lsp.lua
@@ -6,8 +6,10 @@ require("mason").setup()
 require("mason-lspconfig").setup()
 
 vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
-	virtual_text = false,
 	signs = true,
+	underline = false,
+	update_in_insert = false,
+	virtual_text = false,
 })
 
 local buf_set_keymap = vim.api.nvim_buf_set_keymap
