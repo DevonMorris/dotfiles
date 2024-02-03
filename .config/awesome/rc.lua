c25fc5b74608a6d1a58b02aac9ab0001eb2286e2
Devon Morris <devonmorris1992@gmail.com>
2024-02-02 21:38:08 -0500

    Add polkit to awesomewm

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -653,4 +653,4 @@ client.connect_signal("unfocus", function(c) c.border_color = beautiful.border_n
 awful.spawn("setxkbmap -layout us -option ctrl:nocaps")
 awful.spawn("nm-applet")
 awful.spawn("blueman-applet")
--- awful.spawn("picom -b")
+awful.spawn("lxpolkit")
