dbfa5c1955e0d97c2ecc0dd2e378956e8b8b7c88
Devon Morris <devonmorris1992@gmail.com>
2024-02-02 21:36:58 -0500

    Add rust build bindings

---
+++ .config/nvim/ftplugin/rust.lua
@@ -0,0 +1,8 @@
+local buf_set_keymap = vim.api.nvim_buf_set_keymap
+local opts = { noremap = true, silent = true }
+
+vim.o.matchpairs = "(:),{:},[:],<:>"
+vim.o.colorcolumn = "101"
+vim.b.makeprg = "cargo build"
+buf_set_keymap(0, "n", "<leader>y", [[<Cmd>Make check<CR>]], opts)
+buf_set_keymap(0, "n", "<leader>t", [[<Cmd>Make test<CR>]], opts)
