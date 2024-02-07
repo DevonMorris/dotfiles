927158576fe0e6f2f381d07ff82d22ddefb2b3cc
Devon Morris <devonmorris1992@gmail.com>
2024-02-07 09:19:14 -0500

    Make gitsigns a bit less noisy

--- .config/nvim/after/plugin/gitsigns.lua
+++ .config/nvim/after/plugin/gitsigns.lua
@@ -1,46 +1,48 @@
 require("gitsigns").setup({
-	numhl = true,
-	on_attach = function(bufnr)
-		local gs = package.loaded.gitsigns
+    numhl = false,
+    signcolumn = true,
 
-		local function map(mode, l, r, opts)
-			opts = opts or {}
-			opts.buffer = bufnr
-			vim.keymap.set(mode, l, r, opts)
-		end
-		-- Navigation
-		map("n", "]g", function()
-			if vim.wo.diff then
-				return "]c"
-			end
-			vim.schedule(function()
-				gs.next_hunk()
-			end)
-			return "<Ignore>"
-		end, { expr = true })
+    on_attach = function(bufnr)
+        local gs = package.loaded.gitsigns
 
-		map("n", "[g", function()
-			if vim.wo.diff then
-				return "[c"
-			end
-			vim.schedule(function()
-				gs.prev_hunk()
-			end)
-			return "<Ignore>"
-		end, { expr = true })
+        local function map(mode, l, r, opts)
+            opts = opts or {}
+            opts.buffer = bufnr
+            vim.keymap.set(mode, l, r, opts)
+        end
+        -- Navigation
+        map("n", "]g", function()
+            if vim.wo.diff then
+                return "]c"
+            end
+            vim.schedule(function()
+                gs.next_hunk()
+            end)
+            return "<Ignore>"
+        end, { expr = true })
 
-		-- Actions
-		map("n", "<C-G>a", gs.stage_hunk)
-		map("n", "<C-G>r", gs.reset_hunk)
-		map("v", "<C-G>a", function()
-			gs.stage_hunk({ vim.fn.line("."), vim.fn.line("v") })
-		end)
-		map("v", "<C-G>r", function()
-			gs.reset_hunk({ vim.fn.line("."), vim.fn.line("v") })
-		end)
-		map("n", "<C-G>A", gs.stage_buffer)
-		map("n", "<C-G>u", gs.undo_stage_hunk)
-		map("n", "<C-G>R", gs.reset_buffer)
-		map("n", "<C-G>p", gs.preview_hunk)
-	end,
+        map("n", "[g", function()
+            if vim.wo.diff then
+                return "[c"
+            end
+            vim.schedule(function()
+                gs.prev_hunk()
+            end)
+            return "<Ignore>"
+        end, { expr = true })
+
+        -- Actions
+        map("n", "<C-G>a", gs.stage_hunk)
+        map("n", "<C-G>r", gs.reset_hunk)
+        map("v", "<C-G>a", function()
+            gs.stage_hunk({ vim.fn.line("."), vim.fn.line("v") })
+        end)
+        map("v", "<C-G>r", function()
+            gs.reset_hunk({ vim.fn.line("."), vim.fn.line("v") })
+        end)
+        map("n", "<C-G>A", gs.stage_buffer)
+        map("n", "<C-G>u", gs.undo_stage_hunk)
+        map("n", "<C-G>R", gs.reset_buffer)
+        map("n", "<C-G>p", gs.preview_hunk)
+    end,
 })
