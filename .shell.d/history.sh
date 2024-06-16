74e676b9a129de905d6fd236ecdb64ab3a2b68b6
Devon Morris <devonmorris1992@gmail.com>
2024-06-16 15:30:05 -0400

    Add atuin if available

--- .shell.d/history.sh
+++ .shell.d/history.sh
@@ -1,3 +1,11 @@
 export HISTCONTROL=ignoreboth
 export HISTSIZE=5000
 export HISTFILESIZE=10000
+
+[[ -f ~/.bash-preexec.sh ]] && source ~/.bash-preexec.sh
+[[ -x "$(command -v atuin)" ]] && eval "$(atuin init bash)"
+
+function atuin-off {
+    export ATUIN_DB_PATH=/tmp/atuin_dev.db
+    export ATUIN_RECORD_STORE_PATH=/tmp/atuin_records.db
+}
