ed5684da7f9cef6f19836f2eb856ce32c7e5669b
Devon Morris <devonmorris1992@gmail.com>
2025-01-10 10:15:21 -0500

    Add bindings for rust build

--- .config/nvim/ftplugin/rust.lua
+++ .config/nvim/ftplugin/rust.lua
@@ -3,5 +3,12 @@ local opts = { noremap = true, silent = true }
 
 vim.o.colorcolumn = "101"
 vim.b.makeprg = "cargo build"
-buf_set_keymap(0, "n", "<leader>y", [[<Cmd>Make check<CR>]], opts)
-buf_set_keymap(0, "n", "<leader>t", [[<Cmd>Make test<CR>]], opts)
+cargo_check_cmd = "check --profile release --verbose --all-features"
+cargo_test_cmd = "test --profile release --verbose --all-features"
+cargo_doc_cmd = "doc --profile release --verbose --all-features"
+cargo_clippy = "clippy --profile release --verbose --no-deps --all-features"
+
+buf_set_keymap(0, "n", "<F5>", "<Cmd>Make " .. cargo_check_cmd .. "<CR>", opts)
+buf_set_keymap(0, "n", "<F6>", "<Cmd>Make " .. cargo_test_cmd .. "<CR>", opts)
+buf_set_keymap(0, "n", "<F7>", "<Cmd>Make " .. cargo_clippy .. "<CR>", opts)
+buf_set_keymap(0, "n", "<F8>", "<Cmd>Make " .. cargo_doc_cmd .. "<CR>", opts)
