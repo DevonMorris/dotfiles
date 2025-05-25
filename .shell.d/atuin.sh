3bd354b295ab19ed0eb7eda730bb3a60a44521cd
Devon Morris <devonmorris1992@gmail.com>
2025-05-25 15:55:55 -0400

    Search for atuin before executing

--- .shell.d/atuin.sh
+++ .shell.d/atuin.sh
@@ -1,4 +1,5 @@
-[[ -f ~/.bash-preexec.sh ]] && source ~/.bash-preexec.sh
-eval "$(atuin init bash)"
-
-. "$HOME/.atuin/bin/env"
+if which atuin &> /dev/null; then
+    [[ -f ~/.bash-preexec.sh ]] && source ~/.bash-preexec.sh
+    eval "$(atuin init bash)"
+    . "$HOME/.atuin/bin/env"
+fi
