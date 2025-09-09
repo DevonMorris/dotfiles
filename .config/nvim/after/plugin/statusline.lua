264c3a99351706fd8c82a26fd6ffce2ad3f3f06d
Devon Morris <devonmorris1992@gmail.com>
2025-09-09 11:11:31 -0400

    Update colorschemes for alacritty

--- .config/nvim/after/plugin/statusline.lua
+++ .config/nvim/after/plugin/statusline.lua
@@ -1,27 +1,27 @@
 local lualine = require("lualine")
 
 lualine.setup({
-	options = {
-		theme = "gruvbox",
-		section_separators = { nil, nil },
-		component_separators = { nil, nil },
-		icons_enabled = false,
-		globalstatus = true,
-	},
-	sections = {
-		lualine_a = { { "mode", upper = true } },
-		lualine_b = { { "branch", icon = "branch:" } },
-		lualine_c = { { "filename", file_status = true, path = 1 }, { "diagnostics", sources = { "nvim_diagnostic" } } },
-		lualine_x = { "encoding", "filetype" },
-		lualine_y = { "progress" },
-		lualine_z = { "location" },
-	},
-	inactive_sections = {
-		lualine_a = {},
-		lualine_b = {},
-		lualine_c = { "filename" },
-		lualine_x = { "location" },
-		lualine_y = {},
-		lualine_z = {},
-	},
+    options = {
+        theme = "gruvbox",
+        section_separators = { nil, nil },
+        component_separators = { nil, nil },
+        icons_enabled = false,
+        globalstatus = true,
+    },
+    sections = {
+        lualine_a = { { "mode", upper = true } },
+        lualine_b = { { "branch", icon = "branch:" } },
+        lualine_c = { { "filename", file_status = true, path = 1 }},
+        lualine_x = { { "diagnostics", sources = { "nvim_diagnostic" } }, "encoding", "filetype" },
+        lualine_y = { "progress" },
+        lualine_z = { "location" },
+    },
+    inactive_sections = {
+        lualine_a = {},
+        lualine_b = {},
+        lualine_c = { "filename" },
+        lualine_x = { "location" },
+        lualine_y = {},
+        lualine_z = {},
+    },
 })
--- .config/nvim/after/plugin/treesitter.lua
+++ .config/nvim/after/plugin/treesitter.lua
@@ -57,16 +57,3 @@ require("nvim-treesitter.configs").setup({
         },
     },
 })
-
-local ts_repeat_move = require("nvim-treesitter.textobjects.repeatable_move")
-
--- Repeat movement with ; and ,
--- ensure ; goes forward and , goes backward regardless of the last direction
-vim.keymap.set({ "n", "x", "o" }, ";", ts_repeat_move.repeat_last_move_next)
-vim.keymap.set({ "n", "x", "o" }, ",", ts_repeat_move.repeat_last_move_previous)
-
--- Optionally, make builtin f, F, t, T also repeatable with ; and ,
-vim.keymap.set({ "n", "x", "o" }, "f", ts_repeat_move.builtin_f)
-vim.keymap.set({ "n", "x", "o" }, "F", ts_repeat_move.builtin_F)
-vim.keymap.set({ "n", "x", "o" }, "t", ts_repeat_move.builtin_t)
-vim.keymap.set({ "n", "x", "o" }, "T", ts_repeat_move.builtin_T)
