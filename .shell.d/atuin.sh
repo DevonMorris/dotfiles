e686f4a894a79eecf922e3a88377b92867583bcf
Devon Morris <devonmorris1992@gmail.com>
2026-01-30 09:44:11 -0500

    Auto install atuin

--- .shell.d/atuin.sh
+++ .shell.d/atuin.sh
@@ -1,3 +1,6 @@
+# Install atuin if not installed
+[ ! -d $HOME/.atuin ] && curl --proto '=https' --tlsv1.2 -LsSf https://setup.atuin.sh | sh
+
 if which atuin &> /dev/null; then
     [[ -f ~/.bash-preexec.sh ]] && source ~/.bash-preexec.sh
     eval "$(atuin init bash)"
